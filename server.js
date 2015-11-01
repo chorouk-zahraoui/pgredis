380f2d32c9533687eea433d70e743300da6e022d
Chorouk <chorouk.zahraoui@gmail.com>
2015-11-01 20:37:22 +0100

    Initial import

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require('express'),
+    Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('postgres://node:node@52.28.17.216:5432/node');
+var Register = sequelize.define('user_status',{
+    ts: Sequelize.DATE,
+    status: Sequelize.STRING
+});
+sequelize
+    .sync({force: false})
+    .then(function(){
+        console.log('ready')
+    }
+);
+
+var app = express();
+//app.get('/update-status/:stat', function(req,res){
+//    var register = Register.create({
+//        ts: Date(),
+//        status: req.params.stat
+//    });
+//    sequelize
+//        .sync()
+//        .then(function(){
+//            res.send(req.params.stat);
+//        })
+//        .catch(function(err){
+//            res.send(err);
+//        });
+//});
+app.get('/update-status/:stat', function(req,res){
+    Register.create({
+        ts: Date(),
+        status: req.params.stat
+    })
+        .then(function(){
+            res.send(req.params.stat);
+        })
+        .catch(function(err){
+            res.send(err);
+        });
+});
+
+app.listen(8001);
+
+function execute(obj) {
+    return sequelize
+        .sync()
+}
