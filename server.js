83ca9c2ee6e645ce8afd0a4992b0f51f50116347
Chorouk <chorouk.zahraoui@gmail.com>
2015-11-01 20:58:28 +0100

    without postgres

--- server.js
+++ server.js
@@ -9,36 +9,24 @@ var Register = sequelize.define('user_status',{
 sequelize
     .sync({force: false})
     .then(function(){
-        console.log('ready')
+        console.log('ready');
     }
 );
 
 var app = express();
-//app.get('/update-status/:stat', function(req,res){
-//    var register = Register.create({
-//        ts: Date(),
-//        status: req.params.stat
-//    });
-//    sequelize
-//        .sync()
-//        .then(function(){
-//            res.send(req.params.stat);
-//        })
-//        .catch(function(err){
-//            res.send(err);
-//        });
-//});
+
 app.get('/update-status/:stat', function(req,res){
-    Register.create({
-        ts: Date(),
-        status: req.params.stat
-    })
-        .then(function(){
-            res.send(req.params.stat);
-        })
-        .catch(function(err){
-            res.send(err);
-        });
+    //Register.create({
+    //    ts: Date(),
+    //    status: req.params.stat
+    //})
+    //    .then(function(){
+    //        res.send(req.params.stat);
+    //    })
+    //    .catch(function(err){
+    //        res.send(err);
+    //    });
+    res.send('OK');
 });
 
 app.listen(8001);
